b4195fda28d6f441ada2e69e10387407d271926b
ChoiWonYu <93238308+ChoiWonYu@users.noreply.github.com>
2022-05-10 18:12:10 +0900

    Add files via upload

---
+++ src/Fancy.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Fancy(){
+    return(
+        <div>facy</div>
+    )
+}
+export default Fancy;
---
+++ src/Study.js
@@ -0,0 +1,7 @@
+import React from 'react';
+function Study(){
+    return(
+        <div>study</div>
+    )
+}
+export default Study;
